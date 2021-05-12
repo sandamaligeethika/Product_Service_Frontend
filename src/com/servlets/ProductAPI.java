a869d62d0405c0a02d46942e1879c2d5c1f48413
sandamaligeethika <sgeethika98@gmail.com>
2021-05-13 03:21:54 +0530

    servlet API doPost, doDelete operations

--- src/com/servlets/ProductAPI.java
+++ src/com/servlets/ProductAPI.java
@@ -2,6 +2,9 @@ package com.servlets;
 
 import java.io.IOException;
 import java.sql.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -61,6 +64,19 @@ public class ProductAPI extends HttpServlet {
 	protected void doPut(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		productObj = new Product();
 		productDaoObj = new ProductDAOImpl();
+		
+		Map parameters = getParasMap(request);
+		
+		productObj.setProductId(Integer.parseInt(parameters.get("productId").toString()));
+		productObj.setName(parameters.get("productName").toString());
+		productObj.setDate(Date.valueOf(parameters.get("productDate").toString()));
+		productObj.setPrice(Double.parseDouble(parameters.get("productPrice").toString()));
+		productObj.setResId(parameters.get("researcherId").toString());
+		
+		String output = productDaoObj.updateProduct(productObj);
+		
+		response.getWriter().write(output);
+		
 	}
 
 	/**
@@ -68,6 +84,33 @@ public class ProductAPI extends HttpServlet {
 	 */
 	protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		productDaoObj = new ProductDAOImpl();
+		
+		Map parameters = getParasMap(request);
+		
+		String output = productDaoObj.deleteProduct(Integer.parseInt(parameters.get("id").toString()));
+		response.getWriter().write(output);
 	}
-
+	
+	// Convert request parameters to a Map
+	private static Map getParasMap(HttpServletRequest request)
+	{
+		Map<String, String> map = new HashMap<String, String>();
+		try
+		{
+			Scanner scanner = new Scanner(request.getInputStream(), "UTF-8");
+			String queryString = scanner.hasNext() ?
+			scanner.useDelimiter("\\A").next() : "";
+			scanner.close();
+			String[] params = queryString.split("&");
+			for (String param : params)
+			{
+				String[] p = param.split("=");
+				map.put(p[0], p[1]);
+			}
+		}
+		catch (Exception e)
+		{
+		}
+		return map;
+	}
 }
